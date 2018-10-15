b582fab3c04c28090ac11a05754ce67ceaf533b0
zzh <546934282@qq.com>
2018-10-15 15:56:29 +0800

    shiro

---
+++ web_exception_project/web_exception_project/src/main/java/com/luo/exception/BusinessException.java
@@ -0,0 +1,11 @@
+package com.luo.exception;
+
+public class BusinessException extends RuntimeException {
+	
+	private static final long serialVersionUID = 1L;
+
+	public BusinessException(Object Obj) {
+		super(Obj.toString());
+	}
+	
+}
